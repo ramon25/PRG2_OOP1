c371266566161b8c4b780495a6e73cd61a175578
ramon25 <ramon25@gmail.com>
2012-02-24 10:42:30 +0100

    First

---
+++ src/prg2_oop1/Konto.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package prg2_oop1;
+
+/**
+ *
+ * @author ramon
+ */
+public class Konto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
